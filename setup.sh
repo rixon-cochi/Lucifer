3130d26f311d8863852d868e755c8bc4d667523b
Tech cochi <61864331+rixon-cochi@users.noreply.github.com>
2020-08-19 14:43:56 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -12,6 +12,7 @@ apt install ruby -y
 gem install lolcat
 apt install git curl openssh openssl openssl-tool -y
 figlet -f big FINISHED !!! | lolcat
+xdg-open https://www.youtube.com/channel/UCiE0p7rXWBEncUVsLo1C5Xg
 echo
 echo -e "\e[1m Now Run bash instacrack.sh...!!!"
 echo
